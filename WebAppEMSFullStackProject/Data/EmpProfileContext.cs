dd0a0437a1d9405ff0b6bc8d6bb95fb031b34305
Gnanaprasuna <ittamgnanaprasuna1755@gmail.com>
2024-02-10 13:04:44 +0530

    Add project files.

---
+++ WebAppEMSFullStackProject/Data/EmpProfileContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebAppEMSFullStackProject.Models;
+
+namespace WebAppEMSFullStackProject.Data
+{
+    public class EmpProfileContext : DbContext
+    {
+        public EmpProfileContext (DbContextOptions<EmpProfileContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebAppEMSFullStackProject.Models.EmpProfile> EmpProfile { get; set; } = default!;
+    }
+}
